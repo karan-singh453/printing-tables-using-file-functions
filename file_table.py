6ee07545c45880e7cd0113948059afbb9cdb3000
karan-singh453 <71580808+karan-singh453@users.noreply.github.com>
2022-03-04 00:55:10 +0530

    Update file_table.py

--- file_table.py
+++ file_table.py
@@ -1,4 +1,7 @@
+# first loop to create unique file for each table
 for i in range(2,21):
+    #creating file by the name "mul(the tables name).txt" in append mode
     with open(f'mul{i}.txt','a') as f:
+        #second loop to print tables in the text files
         for j in range(1,11):
             f.write(f"{i}*{j}={i*j}\n")
